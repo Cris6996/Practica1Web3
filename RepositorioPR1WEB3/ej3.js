de1a039eb25d8fa4c87e7b60999866065cd53166
and <crisark986@gmail.com>
2025-09-23 17:56:01 -0400

    tercer ejercicio v2

--- RepositorioPR1WEB3/ej3.js
+++ RepositorioPR1WEB3/ej3.js
@@ -19,8 +19,11 @@ function miFuncion(arr) {
   return resultado;
 }
 
-
+console.log("Nombre: Orellana Quispe Cristian Andres, CI: 10954812, Carrera: Informatica, Materia: Programacion Web 3, Fecha: 23/9/25")
+console.log("Solucion al problema 3")
 let obj = miFuncion([1, 2, 3, 4, 5]);
 console.log(obj); 
+
+console.log("Ejemplo Extra: ")
 console.log(miFuncion([10,21,30,41,50,61,70,81,90,101])); 
 
