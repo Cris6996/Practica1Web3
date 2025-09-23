ed238387629d77888a5ae16d69bf55b933d2ad8c
and <crisark986@gmail.com>
2025-09-23 17:54:46 -0400

    segundo ejercicio v2

--- RepositorioPR1WEB3/ej2.js
+++ RepositorioPR1WEB3/ej2.js
@@ -6,10 +6,12 @@ function miFuncion(cadena) {
     // reverse invierte el array
     // join convierte el array a un string
 
-
+console.log("Nombre: Orellana Quispe Cristian Andres, CI: 10954812, Carrera: Informatica, Materia: Programacion Web 3, Fecha: 23/9/25")
+console.log("Solucion al problema 2")
 let cad = miFuncion("abcd");
 console.log(cad); 
 
+console.log("Ejemplo Extra: ")
 console.log(miFuncion("Cristian"));
 
 
